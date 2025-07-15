55422b4f0a535738063afa6b9195c4cd6c55d739
xuqinggang.zh <xuqinggang.zh@bytedance.com>
2025-07-15 19:35:07 +0800

    feat:优化

--- src/components/filter-form/constant.ts
+++ src/components/filter-form/constant.ts
@@ -21,80 +21,86 @@ export const FORM_TEMPLATE_LIST = [
       const startHistItem2 = stocks?.[0]?.hist?.[len - 13];
       const endHistItem2 = stocks?.[0]?.hist?.[len - 4];
       return {
-        dim_conditions: [{
-          disabled: false,
-          is_up_trend: true,
-          // 最近3天
-          range_date: [dayjs(startHistItem?.日期), dayjs(endHistItem?.日期)],
-          dims_threshold: [
-            {
-              // 有一个大阳线 >=3%
-              name: DIM_NAME.BIG_UPPER_LINES,
-              operator: ">=",
-              threshold: 1,
-            },
-            {
-              // 有一个小阴线
-              name: DIM_NAME.SMALL_DOWN_LINES,
-              operator: ">=",
-              threshold: 1,
-            },
-            {
-              // 有一个小阳线
-              name: DIM_NAME.SMALL_UPPER_LINES,
-              operator: ">=",
-              threshold: 1,
-            },
-            {
-              // 有一个阳上影线
-              name: DIM_NAME.YANG_UPPER_SHADOW_LINE,
-              operator: ">=",
-              threshold: 1,
-            },
-            {
-              // 有一个阴上影线
-              name: DIM_NAME.YIN_UPPER_SHADOW_LINE,
-              operator: ">=",
-              threshold: 1,
-            },
-            // 每日换手率 >=1%
-            {
-              name: DIM_NAME.TURNOVER_RATE_PER_DAY,
-              operator: ">=",
-              threshold: 1,
-            },
-            // 每日换手率 <= 10%
-            {
-              name: DIM_NAME.TURNOVER_RATE_PER_DAY,
-              operator: "<=",
-              threshold: 10,
-            },
-            // 曲线趋势度
-            {
-              name: DIM_NAME.TREND_PERCENTAGE,
-              operator: "<=",
-              threshold: 5,
-            },
-          ],
-        }, {
-          disabled: false,
-          is_up_trend: true,
-          range_date: [dayjs(startHistItem2?.日期), dayjs(endHistItem2?.日期)],
-          dims_threshold: [
-            // 每日换手率 >=1%
-            {
-              name: DIM_NAME.TURNOVER_RATE_PER_DAY,
-              operator: ">=",
-              threshold: 1,
-            },
-            // 每日换手率 <= 10%
-            {
-              name: DIM_NAME.TURNOVER_RATE_PER_DAY,
-              operator: "<=",
-              threshold: 10,
-            },
-          ],
-        }],
+        dim_conditions: [
+          {
+            disabled: false,
+            is_up_trend: true,
+            // 最近3天
+            range_date: [dayjs(startHistItem?.日期), dayjs(endHistItem?.日期)],
+            dims_threshold: [
+              {
+                // 有一个大阳线 >=3%
+                name: DIM_NAME.BIG_UPPER_LINES,
+                operator: ">=",
+                threshold: 1,
+              },
+              {
+                // 有一个小阴线
+                name: DIM_NAME.SMALL_DOWN_LINES,
+                operator: ">=",
+                threshold: 1,
+              },
+              {
+                // 有一个小阳线
+                name: DIM_NAME.SMALL_UPPER_LINES,
+                operator: ">=",
+                threshold: 1,
+              },
+              {
+                // 有一个阳上影线
+                name: DIM_NAME.YANG_UPPER_SHADOW_LINE,
+                operator: ">=",
+                threshold: 1,
+              },
+              {
+                // 有一个阴上影线
+                name: DIM_NAME.YIN_UPPER_SHADOW_LINE,
+                operator: ">=",
+                threshold: 1,
+              },
+              // 每日换手率 >=1%
+              {
+                name: DIM_NAME.TURNOVER_RATE_PER_DAY,
+                operator: ">=",
+                threshold: 1,
+              },
+              // 每日换手率 <= 10%
+              {
+                name: DIM_NAME.TURNOVER_RATE_PER_DAY,
+                operator: "<=",
+                threshold: 10,
+              },
+              // 曲线趋势度
+              {
+                name: DIM_NAME.TREND_PERCENTAGE,
+                operator: "<=",
+                threshold: 5,
+              },
+            ],
+          },
+          {
+            disabled: false,
+            is_up_trend: true,
+            range_date: [
+              dayjs(startHistItem2?.日期),
+              dayjs(endHistItem2?.日期),
+            ],
+            dims_threshold: [
+              // 每日换手率 >=1%
+              {
+                name: DIM_NAME.TURNOVER_RATE_PER_DAY,
+                operator: ">=",
+                threshold: 1,
+              },
+              // 每日换手率 <= 10%
+              {
+                name: DIM_NAME.TURNOVER_RATE_PER_DAY,
+                operator: "<=",
+                threshold: 10,
+              },
+            ],
+          },
+        ],
       };
     },
   },
@@ -105,164 +111,222 @@ export const FORM_TEMPLATE_LIST = [
       const endHistItem = stocks?.[0]?.hist?.[len - 1];
       const startHistItem = stocks?.[0]?.hist?.[len - 6];
 
-      const startHistItem2 = stocks?.[0]?.hist?.[len - 13];
-      const endHistItem2 = stocks?.[0]?.hist?.[len - 4];
+      const startHistItem2 = stocks?.[0]?.hist?.[len - 40];
+      const endHistItem2 = stocks?.[0]?.hist?.[len - 6];
       return {
-        dim_conditions: [{
-          disabled: false,
-          is_up_trend: true,
-          // 最近5天
-          range_date: [dayjs(startHistItem?.日期), dayjs(endHistItem?.日期)],
-          dims_threshold: [
-            {
-              // 有3个小阳线
-              name: DIM_NAME.SMALL_UPPER_LINES,
-              operator: ">=",
-              threshold: 3,
-            },
-            {
-              // 有2个小阴线
-              name: DIM_NAME.SMALL_DOWN_LINES,
-              operator: ">=",
-              threshold: 2,
-            },
-            {
-              // 有一个阳上影线
-              name: DIM_NAME.YANG_UPPER_SHADOW_LINE,
-              operator: ">=",
-              threshold: 1,
-            },
-            // 每日换手率 >= 1%
-            {
-              name: DIM_NAME.TURNOVER_RATE_PER_DAY,
-              operator: ">=",
-              threshold: 1,
-            },
-            // 每日换手率 <= 10%
-            {
-              name: DIM_NAME.TURNOVER_RATE_PER_DAY,
-              operator: "<=",
-              threshold: 10,
-            },
-            // 曲线趋势度
-            {
-              name: DIM_NAME.TREND_PERCENTAGE,
-              operator: "<=",
-              threshold: 5,
-            },
-            // 曲线趋势度
-            // {
-            //   name: DIM_NAME.TREND_PERCENTAGE,
-            //   operator: "<=",
-            //   threshold: 5,
-            // },
-          ],
-        }, {
-          disabled: false,
-          is_up_trend: true,
-          range_date: [dayjs(startHistItem2?.日期), dayjs(endHistItem2?.日期)],
-          dims_threshold: [
-            // 每日换手率 >=1%
-            // {
-            //   name: DIM_NAME.TURNOVER_RATE_PER_DAY,
-            //   operator: ">=",
-            //   threshold: 1,
-            // },
-            // 每日换手率 <= 10%
-            {
-              name: DIM_NAME.TURNOVER_RATE_PER_DAY,
-              operator: "<=",
-              threshold: 10,
-            },
-          ],
-        }],
+        dim_conditions: [
+          {
+            disabled: false,
+            is_up_trend: true,
+            // 最近5天
+            range_date: [dayjs(startHistItem?.日期), dayjs(endHistItem?.日期)],
+            dims_threshold: [
+              {
+                // 有3个小阳线
+                name: DIM_NAME.SMALL_UPPER_LINES,
+                operator: ">=",
+                threshold: 3,
+              },
+              {
+                // 有2个小阴线
+                name: DIM_NAME.SMALL_DOWN_LINES,
+                operator: ">=",
+                threshold: 2,
+              },
+              {
+                // 有一个阳上影线
+                name: DIM_NAME.YANG_UPPER_SHADOW_LINE,
+                operator: ">=",
+                threshold: 1,
+              },
+              // 每日换手率 >= 1%
+              {
+                name: DIM_NAME.TURNOVER_RATE_PER_DAY,
+                operator: ">=",
+                threshold: 1,
+              },
+              // 每日换手率 <= 10%
+              {
+                name: DIM_NAME.TURNOVER_RATE_PER_DAY,
+                operator: "<=",
+                threshold: 10,
+              },
+              // 曲线趋势度
+              {
+                name: DIM_NAME.TREND_PERCENTAGE,
+                operator: "<=",
+                threshold: 5,
+              },
+              // 曲线趋势度
+              // {
+              //   name: DIM_NAME.TREND_PERCENTAGE,
+              //   operator: "<=",
+              //   threshold: 5,
+              // },
+            ],
+          },
+          {
+            disabled: false,
+            is_up_trend: true,
+            range_date: [
+              dayjs(startHistItem2?.日期),
+              dayjs(endHistItem2?.日期),
+            ],
+            dims_threshold: [
+              // 每日换手率 >=1%
+              // {
+              //   name: DIM_NAME.TURNOVER_RATE_PER_DAY,
+              //   operator: ">=",
+              //   threshold: 1,
+              // },
+              // 每日换手率 <= 10%
+              {
+                name: DIM_NAME.TURNOVER_RATE_PER_DAY,
+                operator: "<=",
+                threshold: 10,
+              },
+              // 市值 <= 100亿
+              {
+                name: DIM_NAME.MARKET_TOTAL,
+                operator: "<=",
+                threshold: 100,
+              },
+            ],
+          },
+        ],
       };
     },
   },
   {
-    name: "中京电子模板",
+    name: "近5天大阳小阴模板",
     getFormValues: (stocks: IUpStockItemInfo[]) => {
       const len = stocks?.[0]?.hist?.length;
       const endHistItem = stocks?.[0]?.hist?.[len - 1];
       const startHistItem = stocks?.[0]?.hist?.[len - 5];
 
-      const startHistItem2 = stocks?.[0]?.hist?.[len - 13];
-      const endHistItem2 = stocks?.[0]?.hist?.[len - 4];
+      const startHistItem2 = stocks?.[0]?.hist?.[len - 30];
+      const endHistItem2 = stocks?.[0]?.hist?.[len - 10];
       return {
-        dim_conditions: [{
-          disabled: false,
-          is_up_trend: true,
-          // 最近4天
-          range_date: [dayjs(startHistItem?.日期), dayjs(endHistItem?.日期)],
-          dims_threshold: [
-            {
-              // 有一个大阳线 >=3%
-              name: DIM_NAME.BIG_UPPER_LINES,
-              operator: ">=",
-              threshold: 1,
-            },
-            {
-              // 有一个小阴线
-              name: DIM_NAME.SMALL_DOWN_LINES,
-              operator: ">=",
-              threshold: 1,
-            },
-            {
-              // 有一个小阳线
-              name: DIM_NAME.SMALL_UPPER_LINES,
-              operator: ">=",
-              threshold: 1,
-            },
-            {
-              // 有一个阳上影线
-              name: DIM_NAME.YANG_UPPER_SHADOW_LINE,
-              operator: ">=",
-              threshold: 1,
-            },
-            {
-              // 有一个阴上影线
-              name: DIM_NAME.YIN_UPPER_SHADOW_LINE,
-              operator: ">=",
-              threshold: 1,
-            },
-            // 每日换手率 >=2%
-            {
-              name: DIM_NAME.TURNOVER_RATE_PER_DAY,
-              operator: ">=",
-              threshold: 1,
-            },
-            // 每日换手率 <= 10%
-            {
-              name: DIM_NAME.TURNOVER_RATE_PER_DAY,
-              operator: "<=",
-              threshold: 10,
-            },
-            // 曲线趋势度
-            {
-              name: DIM_NAME.TREND_PERCENTAGE,
-              operator: "<=",
-              threshold: 5,
-            },
-          ],
-        }, {
-          disabled: false,
-          is_up_trend: true,
-          range_date: [dayjs(startHistItem2?.日期), dayjs(endHistItem2?.日期)],
-          dims_threshold: [
-            // 每日换手率 >=1%
-            {
-              name: DIM_NAME.TURNOVER_RATE_PER_DAY,
-              operator: ">=",
-              threshold: 1,
-            },
-            // 每日换手率 <= 10%
-            {
-              name: DIM_NAME.TURNOVER_RATE_PER_DAY,
-              operator: "<=",
-              threshold: 10,
-            },
-          ],
-        }],
+        dim_conditions: [
+          {
+            disabled: false,
+            is_up_trend: true,
+            // 最近5天
+            range_date: [dayjs(startHistItem?.日期), dayjs(endHistItem?.日期)],
+            dims_threshold: [
+              {
+                // 有一个大阳线 >=3%
+                name: DIM_NAME.BIG_UPPER_LINES,
+                operator: ">=",
+                threshold: 1,
+              },
+              {
+                // 有一个小阴线
+                name: DIM_NAME.SMALL_DOWN_LINES,
+                operator: ">=",
+                threshold: 2,
+              },
+              {
+                // 有一个小阳线
+                name: DIM_NAME.SMALL_UPPER_LINES,
+                operator: ">=",
+                threshold: 1,
+              },
+              {
+                // 有一个阳上影线
+                name: DIM_NAME.YANG_UPPER_SHADOW_LINE,
+                operator: ">=",
+                threshold: 1,
+              },
+              {
+                // 有一个阴上影线
+                name: DIM_NAME.YIN_UPPER_SHADOW_LINE,
+                operator: ">=",
+                threshold: 1,
+              },
+              // 每日换手率 >=1%
+              {
+                name: DIM_NAME.TURNOVER_RATE_PER_DAY,
+                operator: ">=",
+                threshold: 1,
+              },
+              // 每日换手率 <= 10%
+              {
+                name: DIM_NAME.TURNOVER_RATE_PER_DAY,
+                operator: "<=",
+                threshold: 10,
+              },
+              // 曲线趋势度
+              // {
+              //   name: DIM_NAME.TREND_PERCENTAGE,
+              //   operator: "<=",
+              //   threshold: 5,
+              // },
+            ],
+          },
+          // 最近一天 小阴
+          {
+            disabled: false,
+            is_up_trend: false,
+            range_date: [
+              dayjs(stocks?.[0]?.hist?.[len - 1]?.日期),
+              dayjs(stocks?.[0]?.hist?.[len - 1]?.日期),
+            ],
+            dims_threshold: [
+              {
+                // 有一个小阴线
+                name: DIM_NAME.SMALL_DOWN_LINES,
+                operator: ">=",
+                threshold: 1,
+              },
+            ],
+          },
+          // 最近前一天 小阴
+          {
+            disabled: false,
+            is_up_trend: false,
+            range_date: [
+              dayjs(stocks?.[0]?.hist?.[len - 2]?.日期),
+              dayjs(stocks?.[0]?.hist?.[len - 2]?.日期),
+            ],
+            dims_threshold: [
+              {
+                // 有一个小阴线
+                name: DIM_NAME.SMALL_DOWN_LINES,
+                operator: ">=",
+                threshold: 1,
+              },
+            ],
+          },
+          {
+            disabled: false,
+            is_up_trend: true,
+            range_date: [
+              dayjs(startHistItem2?.日期),
+              dayjs(endHistItem2?.日期),
+            ],
+            dims_threshold: [
+              // 每日换手率 >=1%
+              {
+                name: DIM_NAME.TURNOVER_RATE_PER_DAY,
+                operator: ">=",
+                threshold: 1,
+              },
+              // 每日换手率 <= 10%
+              {
+                name: DIM_NAME.TURNOVER_RATE_PER_DAY,
+                operator: "<=",
+                threshold: 10,
+              },
+              // 市值 <= 100亿
+              {
+                name: DIM_NAME.MARKET_TOTAL,
+                operator: "<=",
+                threshold: 100,
+              },
+            ],
+          },
+        ],
       };
     },
   },
